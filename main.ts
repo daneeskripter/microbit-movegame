7e9f793c378f2ed130ffc0e77dcedfc68ce630f5
daneeskripter <63862773+daneeskripter@users.noreply.github.com>
2020-12-27 10:59:18 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onButtonPressed(Button.A, function () {
+    if (dot_started == 0) {
+        I2C_LCD1602.clear()
+        I2C_LCD1602.ShowString("-", 0, 0)
+        dot_started = 1
+    } else {
+        I2C_LCD1602.shl()
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (started == 1) {
+        I2C_LCD1602.clear()
+        I2C_LCD1602.ShowString("Sleep Mode...", 0, 0)
+        I2C_LCD1602.ShowString("zzz...", 0, 1)
+        basic.pause(2000)
+        I2C_LCD1602.clear()
+        I2C_LCD1602.BacklightOff()
+        started = 0
+    } else {
+        I2C_LCD1602.clear()
+        I2C_LCD1602.BacklightOn()
+        dot_started = 0
+        started = 1
+        I2C_LCD1602.ShowString("MoveGame | Use", 0, 0)
+        I2C_LCD1602.ShowString("A and B buttons", 0, 1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (dot_started == 0) {
+        I2C_LCD1602.clear()
+        I2C_LCD1602.ShowString("-", 0, 0)
+        dot_started = 1
+    } else {
+        I2C_LCD1602.shr()
+    }
+})
+let started = 0
+let dot_started = 0
+I2C_LCD1602.LcdInit(39)
+dot_started = 0
+started = 1
+I2C_LCD1602.ShowString("MoveGame | Use", 0, 0)
+I2C_LCD1602.ShowString("A and B buttons", 0, 1)
